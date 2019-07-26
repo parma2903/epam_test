d2ff33f322246c5a2c4aaa0b08db43a9ef943c5d
Yulia <parma2903@gmail.com>
2019-07-26 10:11:34 +0300

    Переименованы переменные

--- weather.js
+++ weather.js
@@ -2,20 +2,20 @@
 var multiItemSlider = (function () {
     return function (selector, config) {
         var
-            _mainElement = document.querySelector(selector),
-            _sliderWrapper = _mainElement.querySelector('.slider__days'),
-            _sliderItems = _mainElement.querySelectorAll('.slider__item'),
-            _sliderControls = _mainElement.querySelectorAll('.slider__control'),
-            _sliderControlLeft = _mainElement.querySelector('.slider__control--left'),
-            _sliderControlRight = _mainElement.querySelector('.slider__control--right'),
-            _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width),
-            _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width),  
-            _positionLeftItem = 0,
-            _transform = 0,
-            _step = _itemWidth / _wrapperWidth * 100,
-            _items = [];
-        _sliderItems.forEach(function (item, index) {
-            _items.push({
+            mainElement = document.querySelector(selector),
+            sliderWrapper = mainElement.querySelector('.slider__days'),
+            sliderItems = mainElement.querySelectorAll('.slider__item'),
+            sliderControls = mainElement.querySelectorAll('.slider__control'),
+            sliderControlLeft = mainElement.querySelector('.slider__control--left'),
+            sliderControlRight = mainElement.querySelector('.slider__control--right'),
+            wrapperWidth = parseFloat(getComputedStyle(sliderWrapper).width),
+            itemWidth = parseFloat(getComputedStyle(sliderItems[0]).width),  
+            positionLeftItem = 0,
+            transform = 0,
+            step = itemWidth / wrapperWidth * 100,
+            items = [];
+        sliderItems.forEach(function (item, index) {
+            items.push({
                 item: item,
                 position: index,
                 transform: 0
@@ -24,59 +24,59 @@ var multiItemSlider = (function () {
 
         var position = {
             getMin: 0,
-            getMax: _items.length - 1,
+            getMax: items.length - 1,
         }
 
-        var _transformItem = function (direction) {
+        var transformItem = function (direction) {
             if (direction === 'right') {
-                if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) >= position.getMax) {
+                if ((positionLeftItem + wrapperWidth / itemWidth - 1) >= position.getMax) {
                     return;
                 }
-                if (!_sliderControlLeft.classList.contains('slider__control--show')) {
-                    _sliderControlLeft.classList.add('slider__control--show');
+                if (!sliderControlLeft.classList.contains('slider__control--show')) {
+                    sliderControlLeft.classList.add('slider__control--show');
                 }
-                if (_sliderControlRight.classList.contains('slider__control--show') && (_positionLeftItem + _wrapperWidth / _itemWidth) >= position.getMax) {
-                    _sliderControlRight.classList.remove('slider__control--show');
+                if (sliderControlRight.classList.contains('slider__control--show') && (positionLeftItem + wrapperWidth / itemWidth) >= position.getMax) {
+                    sliderControlRight.classList.remove('slider__control--show');
                 }
-                _positionLeftItem++;
-                _transform -= _step;
+                positionLeftItem++;
+                transform -= step;
             }
             if (direction === 'left') {
-                if (_positionLeftItem <= position.getMin) {
+                if (positionLeftItem <= position.getMin) {
                     return;
                 }
-                if (!_sliderControlRight.classList.contains('slider__control--show')) {
-                    _sliderControlRight.classList.add('slider__control--show');
+                if (!sliderControlRight.classList.contains('slider__control--show')) {
+                    sliderControlRight.classList.add('slider__control--show');
                 }
-                if (_sliderControlLeft.classList.contains('slider__control--show') && _positionLeftItem - 1 <= position.getMin) {
-                    _sliderControlLeft.classList.remove('slider__control--show');
+                if (sliderControlLeft.classList.contains('slider__control--show') && positionLeftItem - 1 <= position.getMin) {
+                    sliderControlLeft.classList.remove('slider__control--show');
                 }
-                _positionLeftItem--;
-                _transform += _step;
+                positionLeftItem--;
+                transform += step;
             }
-            _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+            sliderWrapper.style.transform = 'translateX(' + transform + '%)';
         }
 
-        var _controlClick = function (e) {
+        var controlClick = function (e) {
             var direction = this.classList.contains('slider__control--right') ? 'right' : 'left';
             e.preventDefault();
-            _transformItem(direction);
+            transformItem(direction);
         };
 
-        var _setUpListeners = function () {
-            _sliderControls.forEach(function (item) {
-                item.addEventListener('click', _controlClick);
+        var setUpListeners = function () {
+            sliderControls.forEach(function (item) {
+                item.addEventListener('click', controlClick);
             });
         }
 
-        _setUpListeners();
+        setUpListeners();
 
         return {
             right: function () {
-                _transformItem('right');
+                transformItem('right');
             },
             left: function () {
-                _transformItem('left');
+                transformItem('left');
             }
         }
     }
